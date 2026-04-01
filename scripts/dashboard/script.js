2cd7aed7ad60cf3003e7bf640a0435c0a0b6165d
sally <sallymohammed1521999@gmail.com>
2026-04-01 13:21:44 +0200

    fix: update router paths to absolute to fix page loading issues

--- scripts/dashboard/script.js
+++ scripts/dashboard/script.js
@@ -22,20 +22,21 @@ sidebarLink.innerHTML = sidebarItems
   })
   .join("");
 
+
 const router = {
   "/": {
-    html: "pages/dashboard/dashboard.html",
-    js: "scripts/dashboard/dashboard.js",
-    css: "tyle/dashboard/dashboard.css",
+    html: "/pages/dashboard/dashboard.html",
+    js: "/scripts/dashboard/dashboard.js",
+    css: "/style/dashboard/dashboard.css",
   },
-  "/student": { html: "pages/dashboard/student.html" },
-  "/staff": { html: "pages/dashboard/staff.html" },
-  "/academics": { html: "pages/dashboard/academics.html" },
-  "/performance": { html: "pages/dashboard/performance.html" },
-  "/collectFees": { html: "pages/dashboard/collectfees.html"},
-  "/announcement": { html: "pages/dashboard/announcement.html"},
-  "/setup": { html: "pages/dashboard/setup.html" },
-  "/changePassword": { html: "pages/dashboard/changepassword.html"},
+  "/student": { html: "/pages/dashboard/student.html" },
+  "/staff": { html: "/pages/dashboard/staff.html" },
+  "/academics": { html: "/pages/dashboard/academics.html" },
+  "/performance": { html: "/pages/dashboard/performance.html" },
+  "/collectFees": { html: "/pages/dashboard/collectFees.html" },
+  "/announcement": { html: "/pages/dashboard/announcement.html" },
+  "/setup": { html: "/pages/dashboard/setup.html" },
+  "/changePassword": { html: "/pages/dashboard/changePassword.html" },
 };
 
 const route = (event) => {
